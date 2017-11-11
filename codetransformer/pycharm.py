8ec1eefac706e6e2202e089ef4d32075fad5299e
Bryan Thornbury <bt.mozoby@gmail.com>
2017-11-11 12:12:18 -0800

    complete

--- codetransformer/pycharm.py
+++ codetransformer/pycharm.py
@@ -1,17 +1,18 @@
 from transformers.constants import asconstants
 
 z = [1, 2, 3]
+class A(object):
+    def a(s):
+        return len(z)
 
-def a():
-    return len(z)
-
-b = asconstants(z=z)(a)
+b = asconstants(a=A(), z=z)(lambda: a.a())
 
 z = [1,3]
 import dis
 print "Natural:"
-dis.dis(a)
+# dis.dis(a)
 print "\nTransformed:"
 dis.dis(b)
 
+print  b()
 # prints: 2, 3
